9c229b60429d90413d3e4b7b4fec4e761baf72f9
Christopher Chaaya <z5016114@student.unsw.edu.au>
2016-01-20 13:17:33 +1100

    automated commit

--- dilapproject/settings.py
+++ dilapproject/settings.py
@@ -99,12 +99,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'chaayac01@gmail.com'
-# EMAIL_HOST_PASSWORD = 'Jumpplay1'
-# EMAIL_USE_TLS = True
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
